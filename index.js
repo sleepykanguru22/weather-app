6707e545bf08fe0e66ecf322e13bc862facf3796
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-01-19 12:31:45 -0800

    updated app

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-//date & time
-
-let currentTime = new Date();
-
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   let days = [
     "Sunday",
     "Monday",
@@ -12,34 +17,69 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-
-  let currentDay = days[date.getDay()];
-  let currentHours = currentTime.getHours() + ":" + currentTime.getMinutes();
-
-  let formattedDate = ` ${currentDay}, ${currentHours}`;
-
-  return formattedDate;
+  let day = date.getDay();
+  return `${days[day]} ${hours}:${minutes}`;
 }
 
-let time = document.querySelector("#time");
-time.innerHTML = formatDate(currentTime);
-
-//search change city
-function showWeather(response) {
-  document.querySelector("#city-name").innerHTML = response.data.name;
-  document.querySelector(".degrees").innerHTML = `${Math.round(
-    response.data.main.temp
-  )}°<small>C</small>`;
+function displayTemperature(response) {
+  let tempElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let cityWeatherDesc = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let weatherIconElement = document.querySelector("#icon");
+  celciusTemperature = Math.round(response.data.main.temp);
+  tempElement.innerHTML = celciusTemperature;
+  cityElement.innerHTML = response.data.name;
+  cityWeatherDesc.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  weatherIconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function searchForm(event) {
-  event.preventDefault();
+function search(cityName) {
   let apiKey = "4eb395383e9345ff366f409df8a62886";
-  let searchInput = document.querySelector(".search");
-  let city = document.querySelector("#city-name");
-  city.innerHTML = `${searchInput.value}`;
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
-  axios.get(apiURL).then(showWeather);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
-let form = document.querySelector("form");
-form.addEventListener("submit", searchForm);
+
+function changeCity(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+function showFarenheitTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celciusLink.classList.remove("active");
+  farenheitLink.classList.add("active");
+  let farheinheitTemp = (celciusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(farheinheitTemp);
+}
+
+function showCelciusTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celciusLink.classList.add("active");
+  farenheitLink.classList.remove("active");
+  temperatureElement.innerHTML = celciusTemperature;
+}
+
+let celciusTemperature = null;
+search("San Francisco");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", changeCity);
+
+let farenheitLink = document.querySelector("#farenheit-link");
+farenheitLink.addEventListener("click", showFarenheitTemp);
+
+let celciusLink = document.querySelector("#celcius-link");
+celciusLink.addEventListener("click", showCelciusTemp);
